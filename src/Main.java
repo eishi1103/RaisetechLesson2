fb36e66e270093998b9c4160f72453444e62f999
eishi1103 <78992733+eishi1103@users.noreply.github.com>
2022-12-25 15:54:43 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 		Student Eishi = new Student("Eishi Shimada", 24, 70);
 		//beingLazyメソッドで初期のパラメータ(70点)から10点下がる
 		Eishi.beingLazy();
-		//gradeが60点を下回ると留年が確定する
+		//さらに10点下がり、gradeが60点を下回ると留年が確定する
 		Eishi.beingLazy();
 		//留年後、学費の投入が必要になるため、addTuisionメソッドでユーザに任意の額を入れさせる
 		//ユーザが投入できた額が15万円以下なら再受講は諦めるように促す。
